215529e251e1ca51e4594efb5ec8df55fb52637b
Sean Prasertsit <sean@prasertsit.com>
2015-04-24 17:18:53 -0700

    Added some images, changed layouts and headers, added footer

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,9 +6,28 @@ $(function () {
     $(".league-table").tablesorter({
         sortList: [[0, 0]]
     });
+    Ui.setHeaderBackground(Ui.getFirstPath());
+    Ui.setActiveTab(Ui.getFirstPath());
+    $.material.ripples();
 });
 
-if (window.location.href.indexOf("england/1") > 0) {
-    $('.navbar-default.navbar').css('background-color', '#00245D');
-    $('#england-tab').addClass('active');
+function Ui() {
+    this.getFirstPath();
+    this.setHeaderBackground();
+    this.setActiveTab();
 }
+
+Ui.getFirstPath = function() {
+    return window.location.pathname.replace(/^\/([^\/]*).*$/, '$1');
+};
+
+Ui.setHeaderBackground = function(first) {
+    if (first == 'england') $('.navbar-default.navbar').css('background-color', '#00245D');
+    if (first == 'spain') $('.navbar-default.navbar').css('background-color', '#FFDF00');
+    if (first == 'germany') $('.navbar-default.navbar').css('background-color', '#CD0001');
+    if (first == 'italy') $('.navbar-default.navbar').css('background-color', '#317031');
+};
+
+Ui.setActiveTab = function(first) {
+    if (first.length > 0) $('.nav-tabs a[href^="/' + first + '"]').addClass('active');
+};
